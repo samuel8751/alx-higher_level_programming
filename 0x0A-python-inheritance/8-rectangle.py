449ab12b3fcb24bf4f5a0f13eb8d197d08d7fdf3
samuel8751 <samuelgithinji041@gmail.com>
2024-02-05 18:40:21 -0800

    triangle

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,18 +1,68 @@
 #!/usr/bin/python3
+"""
+Module: 8-rectangle
+
+Defines the Rectangle class that inherits from BaseGeometry.
+"""
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-"""
-===================================
-module with class Rectangle
-===================================
-"""
-
-
 class Rectangle(BaseGeometry):
-    """Rectangle class that inherits from BaseGeometry"""
+    """
+    Class: Rectangle
 
+    Represents a rectangle.
+
+    Attributes:
+        __width (int): The width of the rectangle.
+        __height (int): The height of the rectangle.
+
+    Methods:
+        area(): Calculates the area of the rectangle.
+
+    """
     def __init__(self, width, height):
+        """
+        Initializes a Rectangle instance.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+
+        Raises:
+            TypeError: If width or height is not an integer.
+            ValueError: If width or height is not a positive integer.
+
+        """
         self.integer_validator("width", width)
-        self.__width = width
         self.integer_validator("height", height)
+        self.__width = width
         self.__height = height
+
+    def __str__(self):
+        """
+        Returns a string representation of the Rectangle.
+
+        Returns:
+            str: A string representation of the Rectangle.
+
+        """
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+# Example usage
+if __name__ == "__main__":
+    r = Rectangle(3, 5)
+
+    print(r)
+    print(dir(r))
+
+    try:
+        print("Rectangle: {}/{}".format(r.__width, r.__height))
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
+
+    try:
+        r2 = Rectangle(4, True)
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
+
