a616fe457756565355a34223b91c4d9c0f8a4dd0
samuel8751 <samuelgithinji041@gmail.com>
2024-02-05 17:55:59 -0800

    lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,36 +1,12 @@
+#!/usr/bin/python3
+"""
+=============================
+Module with the method lookpu
+=============================
+"""
+
+
 def lookup(obj):
-    """
-    Function: lookup
+    """Function for return the attributes for an object"""
 
-    This function returns a list of available attributes and methods of an object.
-
-    :param obj: The object to inspect
-    :return: A list of strings representing attributes and methods of the object
-
-    Example usage:
-    ```python
-    class MyClass:
-        my_attr = 42
-
-        def my_method(self):
-            pass
-
-    obj = MyClass()
-    print(lookup(obj))
-    ```
-    """
-    return [attr for attr in dir(obj)]
-
-# Example usage
-class MyClass1(object):
-    pass
-
-class MyClass2(object):
-    my_attr1 = 3
-    def my_meth(self):
-        pass
-
-print(lookup(MyClass1))
-print(lookup(MyClass2))
-print(lookup(int))
-
+    return dir(obj)
