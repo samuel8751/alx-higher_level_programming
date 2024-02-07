79e7050518fb18385427951ed8fed71caff75306
samuel8751 <samuelgithinji041@gmail.com>
2024-02-06 17:54:40 -0800

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,8 @@
-#!/usr/bin/python3
-
-
 def read_file(filename=""):
-    with open(filename, "r", encoding="UTF-8") as f:
-        print(f.read(), end="")
+    with open(filename, encoding='utf-8') as file:
+        print(file.read(), end='')
+
+# Test the function
+if __name__ == "__main__":
+    read_file("my_file_0.txt")
+
