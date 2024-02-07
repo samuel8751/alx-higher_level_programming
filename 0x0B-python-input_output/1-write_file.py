c95dc514961a9e9ac23e5011038da9d53e97b768
samuel8751 <samuelgithinji041@gmail.com>
2024-02-06 18:03:03 -0800

    write file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,10 @@
-#!/usr/bin/python3
+def write_file(filename="", text=""):
+    with open(filename, mode='w', encoding='utf-8') as file:
+        file.write(text)
+    return len(text)
 
+# Test the function
+if __name__ == "__main__":
+    nb_characters = write_file("my_first_file.txt", "This School is so cool!\n")
+    print(nb_characters)
 
-def number_of_lines(filename=""):
-    with open(filename, "r", encoding="UTF-8") as f:
-        return len(list(f))
