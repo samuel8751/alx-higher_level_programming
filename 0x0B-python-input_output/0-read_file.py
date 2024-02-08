0cfa721bf9f1af73c1d3e7a5657ff27557fd7fee
samuel8751 <samuelgithinji041@gmail.com>
2024-02-08 02:11:55 +0000

    input and output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,11 @@
-
 #!/usr/bin/python3
+"""Defines read file module """
 
 
-def number_of_lines(filename=""):
-    with open(filename, "r", encoding="UTF-8") as f:
-        return len(list(f))
-
-
+def read_file(filename=""):
+    """
+    read_file function
+    reads a text file (UTF8) and prints it result to stdout
+    """
+    with open(filename, mode='r', encoding='utf-8') as f:
+        print(f.read(), end='')
